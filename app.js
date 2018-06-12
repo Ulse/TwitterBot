c0e186689104d4da9b6692290635738664d7e572
Uljana <uljanasejko@gmail.com>
2018-06-12 01:59:53 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const Twitter = require('twitter');
+const config = require('./config.js');
+const T = new Twitter(config);
+
+// Setting up the search params
+const params = {
+  q: '#nodejs',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+//Initiating the search using params above
+// npm Twitter module helps make a get equest to 'search/tweets
+// via data.statuses obj, get request will return array of tweets
+T.get('search/tweets', params, (err, data, response) => {
+  // If there is no error, proceed
+  if(err){
+    return console.log(err);
+  }
+
+  // Loop through the returned tweets
+  const tweetsId = data.statuses
+    .map(tweet => ({ id: tweet.id_str }));
+
+  tweetsId.map(tweetId => {
+    T.post('favorites/create', tweetId, (err, response) => {
+      if(err){
+        return console.log(err[0].message);
+      }
+
+      const username = response.user.screen_name;
+      const favoritedTweetId = response.id_str;
+      console.log(`Favorited: https://twitter.com/${username}/status/${favoritedTweetId}`);
+
+    });
+  });
+})
---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+  consumer_key: 'inLGyjz3lV6UCBCJOHRpe990L',
+  consumer_secret: 'z4NfqU6QDm9IhQNGqfy8ncngIwd0tQTQCDfHfmN4Jd2vqFw85u',
+  access_token_key: '171975021-afvbQW1oq1RO4sMf0oVqKJswPbODgCMhCgjK54aA',
+  access_token_secret: 'P47AJdAkthCWecDPUmltultJPidQnVJlC55usLNcClfIn'
+}
